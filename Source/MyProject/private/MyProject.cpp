38b8014a29dbcb9f5a32a95d78f9dc0155a91cde
motical <motical9833@gmail.com>
2025-05-16 03:17:20 +0900

    프로젝트 기본 세팅

--- Source/MyProject/private/MyProject.cpp
+++ Source/MyProject/private/MyProject.cpp
@@ -4,3 +4,5 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProject, "MyProject" );
+
+DEFINE_LOG_CATEGORY(TPS);
---
+++ Source/MyProject/private/MyTPSGameModeBase.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyTPSGameModeBase.h"
+#include "MyProject.h"
+
+AMyTPSGameModeBase::AMyTPSGameModeBase()
+{
+	PRINT_LOG(TEXT("My Log : %s"), TEXT("TPS project!!"));
+}
